0fc58a27cf9c97bd96da46e1df1e647214d17885
may20334 <may20334@gmail.com>
2019-11-19 18:46:44 +0700

    Add 1 file. change 1 file

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char name[50], G[5], answer[10];
+{ char Name[50], G[5], answer[10];
   int i;
   do
   {
     printf("What is your Name? ");
-    scanf("%s", name);
+    scanf("%s", Name);
     do
     {
       printf("What is your blood group [A, B, AB, or O]? ");
